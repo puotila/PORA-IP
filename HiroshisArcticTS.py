08ec2033b0db549dfd37d1bdf23aeafe4a160ccf
Petteri Uotila <petteri.uotila@fmi.fi>
2016-08-22 15:02:39 +0300

    fine tuning and added Hiroshis

---
+++ HiroshisArcticTS.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""
+Read Hiroshis (AWI) Arctic T, S climatology
+based on recent cruises. Essentially replaces PHC3.
+"""
+
+import numpy as np
+import netCDF4 as nc
+
+class Hiroshis(object):
+    def __init__(self):
+
+if __name__ == "__main__":
+    hrs = Hiroshis()
+    print "Finnished!"
